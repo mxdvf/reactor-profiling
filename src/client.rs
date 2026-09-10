8acbfe9f0d91bb1b65c83944b07484f053173083
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2026-09-10 16:32:35 +0530

    testing client -> server throughput

--- src/client.rs
+++ src/client.rs
@@ -1,102 +1,53 @@
 use crate::common::{Msg, Request};
-
-use base_client;
-
 use reactor_actor::codec::BincodeCodec;
-use reactor_actor::{BehaviourBuilder, RouteTo, RuntimeCtx, SendErrAction};
-
+use reactor_actor::{
+    ActorProcess, ActorSend, BehaviourBuilder, RouteTo, RuntimeCtx, SendErrAction,
+};
 use std::collections::HashMap;
 
-// //////////////////////////////////////////////////////////////////////////////
-//                                  REQUEST GENERATOR
-// //////////////////////////////////////////////////////////////////////////////
+struct Processor;
 
-fn make_request(client_addr: &str, slot_id: usize) -> Msg {
-    Msg::Request(Request {
-        client_addr: client_addr.to_string(),
-        key: slot_id as u64,
-        value: 0,
-        slot_id,
-    })
-}
-
-// //////////////////////////////////////////////////////////////////////////////
-//                                  PROCESSOR
-// //////////////////////////////////////////////////////////////////////////////
-
-struct Processor {
-    benchmark: base_client::Benchmark,
-}
-
-impl reactor_actor::ActorProcess for Processor {
+impl ActorProcess for Processor {
     type IMsg = Msg;
     type OMsg = Msg;
 
-    fn process(&mut self, input: Self::IMsg) -> Vec<Self::OMsg> {
-        match input {
-            Msg::Request(request) => {
-                self.benchmark.on_initial_request(request.slot_id);
-
-                vec![Msg::Request(request)]
-            }
-
-            Msg::Response(response) => {
-                let slot_id = response.slot_id;
-
-                if self.benchmark.on_response(slot_id) {
-                    vec![make_request(self.benchmark.client_addr(), slot_id)]
-                } else {
-                    vec![]
-                }
-            }
-        }
+    fn process(&mut self, input: Msg) -> Vec<Msg> {
+        vec![input]
     }
 }
-
-// //////////////////////////////////////////////////////////////////////////////
-//                                  SENDER
-// //////////////////////////////////////////////////////////////////////////////
 
 struct Sender {
     server: String,
 }
 
-impl reactor_actor::ActorSend for Sender {
+impl ActorSend for Sender {
     type OMsg = Msg;
 
-    async fn before_send<'a>(&'a mut self, output: &Self::OMsg) -> RouteTo<'a> {
-        match output {
-            Msg::Request(_) => RouteTo::from(self.server.as_str()),
-
-            Msg::Response(_) => {
-                panic!("Client tried to send a response message")
-            }
-        }
+    async fn before_send<'a>(&'a mut self, _: &Msg) -> RouteTo<'a> {
+        RouteTo::from(self.server.as_str())
     }
 }
 
-impl Sender {
-    fn new(server: String) -> Self {
-        Self { server }
-    }
-}
+pub async fn client(ctx: RuntimeCtx, server: String, _payload: HashMap<String, serde_json::Value>) {
+    std::thread::sleep(std::time::Duration::from_secs(3));
 
-// //////////////////////////////////////////////////////////////////////////////
-//                                  ACTOR
-// //////////////////////////////////////////////////////////////////////////////
+    let client_addr = ctx.addr.to_string();
 
-pub async fn client(ctx: RuntimeCtx, server: String, payload: HashMap<String, serde_json::Value>) {
-    let benchmark = base_client::Benchmark::new(ctx.addr.to_string(), payload);
-    let workload = benchmark.workload(make_request);
+    let workload = std::iter::repeat_with(move || {
+        Msg::Request(Request {
+            client_addr: client_addr.clone(),
+            key: 100000,
+            value: 10000001,
+            slot_id: 100,
+        })
+    });
 
-    BehaviourBuilder::new(Processor { benchmark }, BincodeCodec::default())
-        .send(Sender::new(server))
+    BehaviourBuilder::new(Processor, BincodeCodec::default())
+        .send(Sender { server })
         .generator_if(true, || workload)
         .on_send_failure(SendErrAction::Drop)
         .build()
         .run(ctx)
         .await
         .unwrap();
-
-    println!("EXPERIMENT COMPLETED, YOU CAN EXIT NOW.");
 }
--- src/server.rs
+++ src/server.rs
@@ -1,26 +1,42 @@
-use crate::common::{Msg, Response};
+use crate::common::Msg;
 use reactor_actor::codec::BincodeCodec;
-use reactor_actor::{BehaviourBuilder, RouteTo, RuntimeCtx};
+use reactor_actor::{ActorProcess, ActorRecv, BehaviourBuilder, ChannelAction, RuntimeCtx};
+use std::time::{Duration, Instant};
 
 struct Processor;
 
-impl reactor_actor::ActorProcess for Processor {
+impl ActorProcess for Processor {
     type IMsg = Msg;
     type OMsg = Msg;
 
-    fn process(&mut self, input: Self::IMsg) -> Vec<Self::OMsg> {
-        match input {
-            Msg::Request(request) => {
-                vec![Msg::Response(Response {
-                    client_addr: request.client_addr,
-                    slot_id: request.slot_id,
-                })]
-            }
+    fn process(&mut self, _: Msg) -> Vec<Msg> {
+        vec![]
+    }
+}
 
-            Msg::Response(_) => {
-                panic!("Server received a response message")
+#[derive(Default)]
+struct Receiver {
+    started: Option<Instant>,
+    received: u64,
+    reported: bool,
+}
+
+impl ActorRecv for Receiver {
+    type IMsg = Msg;
+
+    async fn after_recv(&mut self, _: &str, input: &Msg) -> ChannelAction {
+        if !self.reported && matches!(input, Msg::Request(_)) {
+            let started = self.started.get_or_insert_with(Instant::now);
+            self.received += 1;
+
+            if started.elapsed() >= Duration::from_secs(5) {
+                println!("Requests received in ~60s: {}", self.received);
+                println!("Throughput is: {}", self.received / 5);
+                self.reported = true;
             }
         }
+
+        ChannelAction::PASS
     }
 }
 
@@ -29,22 +45,19 @@ struct Sender;
 impl reactor_actor::ActorSend for Sender {
     type OMsg = Msg;
 
-    async fn before_send<'a>(&'a mut self, output: &Self::OMsg) -> RouteTo<'a> {
-        match output {
-            Msg::Response(response) => RouteTo::from(response.client_addr.clone()),
-
-            Msg::Request(_) => {
-                panic!("Server tried to send a request message")
-            }
-        }
+    async fn before_send<'a>(&'a mut self, _: &Self::OMsg) -> reactor_actor::RouteTo<'a> {
+        panic!("Should not reach here");
     }
 }
 
 pub async fn server(ctx: RuntimeCtx) {
+    println!("yo1?");
     BehaviourBuilder::new(Processor, BincodeCodec::default())
+        .recv(Receiver::default())
         .send(Sender)
         .build()
         .run(ctx)
         .await
         .unwrap();
+    println!("yo2?");
 }
