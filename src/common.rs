3bbb6869770df7ddd40039667030133bcaac48d4
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2026-09-16 18:10:19 +0530

    default impl

--- src/common.rs
+++ src/common.rs
@@ -8,9 +8,25 @@ pub struct Request {
     pub slot_id: usize,
 }
 
+impl Default for Request {
+    fn default() -> Self {
+        Self {
+            key: 123456789,
+            value: 987654321,
+            slot_id: 42,
+        }
+    }
+}
+
 #[derive(Encode, Decode, Debug, Clone)]
 pub struct Response {
     pub slot_id: usize,
+}
+
+impl Default for Response {
+    fn default() -> Self {
+        Self { slot_id: 42 }
+    }
 }
 
 #[derive(Encode, Decode, Debug, Clone, DefaultPrio, DeriveMsg)]
